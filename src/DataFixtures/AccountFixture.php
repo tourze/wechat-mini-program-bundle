8774208502ea28df7e687ff74a05d3ef84729ff3
github-ci <you@example.com>
2025-04-21 13:54:14 +0000

    feat(data-fixtures): 添加 AccountFixture 并更新 composer 依赖

---
+++ src/DataFixtures/AccountFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tourze\WechatMiniProgramCoreBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Tourze\WechatMiniProgramCoreBundle\Entity\Account;
+
+class AccountFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $account = new Account();
+        $account->setName('测试小程序');
+        $account->setValid(true);
+        $account->setAppId('TEST2');
+        $account->setAppSecret('TEST1');
+        $manager->persist($account);
+        $manager->flush();
+    }
+}
