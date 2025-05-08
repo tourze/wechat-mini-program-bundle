7624ed90bb4a84b7b2f17b794cc012b29f78f941
github-ci <you@example.com>
2025-05-08 16:09:56 +0000

    fix+++++++++

--- src/WechatMiniProgramBundle.php
+++ src/WechatMiniProgramBundle.php
@@ -6,4 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class WechatMiniProgramBundle extends Bundle
 {
+    const DEFAULT_AVATAR = 'https://thirdwx.qlogo.cn/mmopen/vi_32/XA9Dx0Gv5dT0Cqic3Ghmt7ftVU8VhicGQiaTZdMS63ojMy7yF5PmXta7DGicW0Uge53TbiakdrNdaiaTiarTaGQH4JY7Q/132';
 }
